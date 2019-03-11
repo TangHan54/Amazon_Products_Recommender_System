7a63d53ba48811c548e4e42d408adb1664e28224
TangHan <e0232190@u.nus.edu.sg>
2019-03-11 12:29:40 +0800

    added some info

--- manage.py
+++ manage.py
@@ -2,18 +2,23 @@ from utils import preprocess_data
 import logging
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
 
 foldername = 'data'
 
-logger.info('Started to unzip raw data.')
+logger.warn('Started to unzip raw data.')
 try:
     preprocess_data.unzip_file(foldername)
 except FileNotFoundError:
-    logger.warning('No raw files exists.')
+    logger.warn('No raw files exists.')
 
-logger.info('Started to process data.')
+logger.warn('Started to process data.')
 try:
     df_product_category, df_rating = preprocess_data.process_data(foldername)
 except FileNotFoundError:
-    logger.warning('No data exists.')
+    logger.warn('No data exists.')
+    
+print('df_product_category has ' + str(df_product_category.count()) +' rows.')
+print('df_rating has ' + str(df_rating.count()) +' rows.')
+
+logger.warn('Data is ready.')
+
